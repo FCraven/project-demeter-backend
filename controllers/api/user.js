d392758f0f037c1e7101332bbefd207cc3724671
FCraven <craven.francis@gmail.com>
2021-06-08 14:57:39 -0400

    added CRUD routes for user model and it is working with db

---
+++ controllers/api/user.js
@@ -0,0 +1,63 @@
+const { User } = require('../../db');
+
+const router = require('express').Router();
+
+// /api/users
+
+router.get('/', async (req,res,next)=> {
+  try {
+    const users = await User.find({},'-password');
+    if(!users) {
+      res.send('No users found')
+    }
+    res.json(users)
+  }catch(err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req,res,next)=> {
+  try {
+    const user = await User.findById(req.params.id, '-password')
+    res.json(user)
+  } catch(err) {
+      next(err)
+  }
+})
+
+router.post('/', async (req,res,next) => {
+  try {
+    const newUser = await User.create(req.body)
+    res.json(newUser)
+  } catch(err) {
+      next(err)
+  }
+})
+
+router.put('/:id', async (req,res,next)=> {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, select: '-password'})
+    if(!updatedUser) {
+      res.send('There was a problem updating the user. Please try again.')
+    }
+    res.json(updatedUser)
+  } catch (err) {
+      next(err)
+  }
+})
+
+
+router.delete('/:id', async (req,res,next)=> {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id, {select: '-password'});
+    if(!deletedUser) {
+      return res.send('There is no user by that id to delete.')
+    }
+    res.json(deletedUser)
+  } catch(err) {
+      next(err)
+  }
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,48 +5,27 @@ const morgan = require('morgan')
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose')
 const { User } = require('./db')
+const routes = require('./controllers')
 
 
-process.env.NODE_ENV === 'production' ?
+if(process.env.NODE_ENV === 'production') {
   mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-:
+} else {
   mongoose.connect('mongodb://localhost/project-demeter-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+}
 
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true})),
 
-app.get('/', (req,res,next)=> {
-  res.send('<h1>Basic server get route at / </h1>')
-})
-
-app.get('/api/users', async (req,res,next)=> {
-  try {
-    const users = await User.find({});
-    res.json(users)
-  }catch(err) {
-    next(err)
-  }
-})
-
-app.post('/api/users', async (req,res,next) => {
-  try {
-    const userData = req.body;
-    const newUser = await User.create(userData)
-    console.log('new User --> ', newUser)
-    res.json(newUser)
-  } catch(err) {
-      next(err)
-  }
-})
-
+app.use(routes)
 
 app.listen(PORT, ()=> {
   console.log(`Server listening on https://localhost:${PORT}`)
