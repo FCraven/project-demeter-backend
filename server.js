5e8c086efff42989d43401e64cdbcaca88495bdb
FCraven <craven.francis@gmail.com>
2021-06-07 20:54:24 -0400

    mongodb accepting queries locally from server

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan')
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose')
+const { User } = require('./db')
+
+
+process.env.NODE_ENV === 'production' ?
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+:
+  mongoose.connect('mongodb://localhost/project-demeter-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
 app.use(morgan('dev'))
 app.use(express.json());
@@ -10,6 +25,26 @@ app.use(express.urlencoded({extended: true})),
 
 app.get('/', (req,res,next)=> {
   res.send('<h1>Basic server get route at / </h1>')
+})
+
+app.get('/api/users', async (req,res,next)=> {
+  try {
+    const users = await User.find({});
+    res.json(users)
+  }catch(err) {
+    next(err)
+  }
+})
+
+app.post('/api/users', async (req,res,next) => {
+  try {
+    const userData = req.body;
+    const newUser = await User.create(userData)
+    console.log('new User --> ', newUser)
+    res.json(newUser)
+  } catch(err) {
+      next(err)
+  }
 })
 
 
