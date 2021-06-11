0f8ac2096dd014a475f9fab61709239792471966
FCraven <craven.francis@gmail.com>
2021-06-11 16:30:06 -0400

    adding home route to prod serv

--- controllers/index.js
+++ controllers/index.js
@@ -3,4 +3,8 @@ const apiRoutes = require('./api')
 
 router.use('/api', apiRoutes)
 
+router.get('/', (req,res)=> {
+  res.send('<h1>From the production server</h1>')
+})
+
 module.exports = router;
